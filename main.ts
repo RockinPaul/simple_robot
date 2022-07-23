06169e994f249c7b8cf21c6544c913c43f15ec22
Pavel Zarudnev <rockinpaulz@gmail.com>
2022-07-23 22:47:31 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,34 @@
+function turnLeft (time: number) {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, 100)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 100)
+    basic.pause(time)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
+}
+input.onButtonPressed(Button.A, function () {
+	
+})
+function reverse (Time: number) {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, 100)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, 100)
+    basic.pause(Time)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
+}
+function turnRight (time: number) {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Reverse, 100)
+    basic.pause(time)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
+}
+function driveForward (Time: number) {
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
+    kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 100)
+    basic.pause(Time)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
+    kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
+}
+basic.forever(function () {
+	
+})
