a7c130f5f99aadb020500e2dde0bc87d5818facd
Pavel Zarudnev <rockinpaulz@gmail.com>
2022-07-24 00:00:55 +0200

    Assembled some sequence of functions

--- main.ts
+++ main.ts
@@ -6,7 +6,11 @@ function turnLeft (time: number) {
     kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
 }
 input.onButtonPressed(Button.A, function () {
-	
+    driveForward(300)
+    turnRight(200)
+    driveForward(300)
+    turnLeft(200)
+    reverse(300)
 })
 function reverse (Time: number) {
     kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, 100)
