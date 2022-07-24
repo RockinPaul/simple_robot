f83c14945327525f6abc983b1b595341bd02ad48
Pavel Zarudnev <rockinpaulz@gmail.com>
2022-07-24 22:06:05 +0200

    Made use of function. Endless run for algorithm on button B

--- main.ts
+++ main.ts
@@ -6,11 +6,13 @@ function turnLeft (time: number) {
     kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
 }
 input.onButtonPressed(Button.A, function () {
-    driveForward(300)
-    turnRight(200)
-    driveForward(300)
-    turnLeft(200)
-    reverse(300)
+    driveForward(2000)
+    turnRight(400)
+    driveForward(1000)
+    turnLeft(400)
+    driveForward(1500)
+    turnLeft(400)
+    driveForward(2000)
 })
 function reverse (Time: number) {
     kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Reverse, 100)
@@ -26,6 +28,17 @@ function turnRight (time: number) {
     kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor1)
     kitronik_klip_motor.motorOff(kitronik_klip_motor.Motors.Motor2)
 }
+input.onButtonPressed(Button.B, function () {
+    while (true) {
+        driveForward(2000)
+        turnRight(400)
+        driveForward(1000)
+        turnLeft(400)
+        driveForward(1500)
+        turnLeft(400)
+        driveForward(2000)
+    }
+})
 function driveForward (Time: number) {
     kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor1, kitronik_klip_motor.MotorDirection.Forward, 100)
     kitronik_klip_motor.motorOn(kitronik_klip_motor.Motors.Motor2, kitronik_klip_motor.MotorDirection.Forward, 100)
